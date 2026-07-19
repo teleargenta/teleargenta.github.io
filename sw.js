520089b97586809278d38eab5aa9524e31da0a67
teleargenta <betomatodo@gmail.com>
2026-07-19 02:40:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    );
-});
+
